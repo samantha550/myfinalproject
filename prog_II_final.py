aeba75eeb4f2184cee2bd01cdc0883f213625cca
Samantha Sanchez <sas550@georgetown.edu>
2022-12-13 19:45:19 -0800

    added hello for testing

--- prog_II_final.py
+++ prog_II_final.py
@@ -2,17 +2,4 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
- educ = st.selectbox("Education level", 
-              options = ["High School Diploma",
-                         "College Degree",
-                         "Graduate Degree"])
- st.write(f"Education (pre-conversion): {educ}")
- st.write("**Convert Selection to Numeric Value**")
- if educ == "High School Diploma":
-     educ = 1
- elif educ == "College Degree":
-     educ = 2
- else:
-     educ = 3
-    
- st.write(f"Education (post-conversion): {educ}")
+print("hello")
