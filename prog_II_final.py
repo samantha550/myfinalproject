b0fd27476a530ba1bacaaf21d767d28f6a8737e1
Samantha Sanchez <sas550@georgetown.edu>
2022-12-13 19:18:00 -0800

    added data section

--- prog_II_final.py
+++ prog_II_final.py
@@ -1,6 +1,18 @@
-import streamlit as st
+import numpy as np
 import pandas as pd
+import streamlit as st
 
-sm_data = pd.read_csv('/Users/samsa/OneDrive/Desktop/Prog II/social_media_usage.csv')
-
-"test"
+ educ = st.selectbox("Education level", 
+              options = ["High School Diploma",
+                         "College Degree",
+                         "Graduate Degree"])
+ st.write(f"Education (pre-conversion): {educ}")
+ st.write("**Convert Selection to Numeric Value**")
+ if educ == "High School Diploma":
+     educ = 1
+ elif educ == "College Degree":
+     educ = 2
+ else:
+     educ = 3
+    
+ st.write(f"Education (post-conversion): {educ}")
